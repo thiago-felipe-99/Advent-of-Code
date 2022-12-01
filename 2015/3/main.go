b1ac9dbbd052238d8db372e7e493d217618c522f
Thiago Felipe <thiago.felipe@tutanota.com>
2022-12-01 04:56:26 -0300

    Resolution of the 2nd puzzle of the day 3 of 2015

--- 2015/3/main.go
+++ 2015/3/main.go
@@ -47,6 +47,63 @@ func puzzle1() int {
 	return len(housesAlreadyVisited)
 }
 
+func puzzle2() int {
+	currentHouse := houseLocation{0, 0}
+	currentHouseSanta := houseLocation{0, 0}
+	currentHouseRobot := houseLocation{0, 0}
+	housesAlreadyVisited := []houseLocation{currentHouse}
+	turnSanta := true
+
+	for _, character := range input {
+		if turnSanta {
+			currentHouse.x = currentHouseSanta.x
+			currentHouse.y = currentHouseSanta.y
+		} else {
+			currentHouse.x = currentHouseRobot.x
+			currentHouse.y = currentHouseRobot.y
+		}
+
+		switch character {
+		case '^':
+			currentHouse.y++
+		case 'v':
+			currentHouse.y--
+		case '>':
+			currentHouse.x++
+		case '<':
+			currentHouse.x--
+		}
+
+		if turnSanta {
+			currentHouseSanta.x = currentHouse.x
+			currentHouseSanta.y = currentHouse.y
+		} else {
+			currentHouseRobot.x = currentHouse.x
+			currentHouseRobot.y = currentHouse.y
+		}
+
+		turnSanta = !turnSanta
+
+		visited := false
+
+		for _, house := range housesAlreadyVisited {
+			if currentHouse.y == house.y && currentHouse.x == house.x {
+				visited = true
+
+				break
+			}
+		}
+
+		if !visited {
+			housesAlreadyVisited = append(housesAlreadyVisited, currentHouse)
+		}
+	}
+
+	// fmt.Println(housesAlreadyVisited)
+	return len(housesAlreadyVisited)
+}
+
 func main() {
 	fmt.Println(puzzle1())
+	fmt.Println(puzzle2())
 }
