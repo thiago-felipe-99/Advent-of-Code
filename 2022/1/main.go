ab17dd70316d13f6a48057a158ddc7c17bf9a2d5
Thiago Felipe <thiago.felipe@tutanota.com>
2022-12-01 03:07:31 -0300

    Resolution of the 2nd puzzle of the day 1 of 2022

--- 2022/1/main.go
+++ 2022/1/main.go
@@ -45,6 +45,47 @@ func puzzle1() int {
 	return maxSum
 }
 
+func puzzle2() int {
+	top3calories := [3]int{0, 0, 0}
+	sumCalories := 0
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			if sumCalories > top3calories[0] { //nolint: gocritic
+				top3calories[2] = top3calories[1]
+				top3calories[1] = top3calories[0]
+				top3calories[0] = sumCalories
+			} else if sumCalories > top3calories[1] {
+				top3calories[2] = top3calories[1]
+				top3calories[1] = sumCalories
+			} else if sumCalories > top3calories[2] {
+				top3calories[2] = sumCalories
+			}
+
+			sumCalories = 0
+
+			continue
+		}
+
+		calories, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+
+		sumCalories += calories
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	return top3calories[0] + top3calories[1] + top3calories[2]
+}
+
 func main() {
 	fmt.Println(puzzle1())
+	fmt.Println(puzzle2())
 }
