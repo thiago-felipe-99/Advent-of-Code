766cda5b7a2f370c81c5b348d06324145424e4c1
Thiago Felipe <thiago.felipe@tutanota.com>
2022-12-01 06:50:49 -0300

    Resolution of the 2nd puzzle of the day 6 of 2015

--- 2015/6/main.go
+++ 2015/6/main.go
@@ -73,6 +73,53 @@ func puzzle1() int {
 	return sum
 }
 
+func puzzle2() int {
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	grid := [gridLen][gridLen]int{}
+	sum := 0
+
+	var function func(int) int
+
+	turnOn := func(brightness int) int { return brightness + 1 }
+	turnOff := func(brightness int) int {
+		if brightness <= 0 {
+			return 0
+		}
+		return brightness - 1
+	}
+	toggle := func(brightness int) int { return brightness + 2 }
+
+	for scanner.Scan() {
+		commands := strings.Split(scanner.Text(), " ")
+
+		if commands[0] == "toggle" {
+			function = toggle
+		} else if commands[1] == "on" {
+			function = turnOn
+		} else {
+			function = turnOff
+		}
+
+		xStart, yStart := getPoint(commands[len(commands)-3])
+		xEnd, yEnd := getPoint(commands[len(commands)-1])
+
+		for x := xStart; x <= xEnd; x++ {
+			for y := yStart; y <= yEnd; y++ {
+				grid[x][y] = function(grid[x][y])
+			}
+		}
+	}
+
+	for x := 0; x < gridLen; x++ {
+		for y := 0; y < gridLen; y++ {
+			sum += grid[x][y]
+		}
+	}
+
+	return sum
+}
+
 func main() {
 	fmt.Println(puzzle1())
+	fmt.Println(puzzle2())
 }
