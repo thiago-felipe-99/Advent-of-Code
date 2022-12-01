1dd951a0bf6887eed55e92fa60abba62d7a93504
Thiago Felipe <thiago.felipe@tutanota.com>
2022-12-01 06:00:25 -0300

    Resolution of the 2nd puzzle of the day 5 of 2015

--- 2015/5/main.go
+++ 2015/5/main.go
@@ -21,21 +21,21 @@ func puzzle1() int {
 		vowels := 0
 		nice := false
 
-		for indice, character := range line {
+		for index, character := range line {
 			if character == 'a' || character == 'e' ||
 				character == 'i' || character == 'o' || character == 'u' {
 				vowels++
 			}
 
-			if indice+1 >= lenLine {
+			if index+1 >= lenLine {
 				continue
 			}
 
-			if line[indice] == line[indice+1] {
+			if line[index] == line[index+1] {
 				nice = true
 			}
 
-			nextCharacter := line[indice : indice+2]
+			nextCharacter := line[index : index+2]
 			if nextCharacter == "ab" || nextCharacter == "cd" ||
 				nextCharacter == "pq" || nextCharacter == "xy" {
 				nice = false
@@ -52,6 +52,61 @@ func puzzle1() int {
 	return quantitesNiceStrings
 }
 
+func puzzle2() int {
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	quantitesNiceStrings := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lenLine := len(line)
+		twoLetters := map[string]struct {
+			lastIndex, count int
+		}{}
+		niceLetterBetween := false
+		niceTwoLetters := false
+
+		for index := range line {
+			if index+1 >= lenLine {
+				continue
+			}
+
+			twoLetter := line[index : index+2]
+			if value, isMapContainsKey := twoLetters[twoLetter]; isMapContainsKey {
+				if value.lastIndex < index-1 {
+					value.count++
+					value.lastIndex = index
+					twoLetters[twoLetter] = value
+				}
+			} else {
+				twoLetters[twoLetter] = struct {
+					lastIndex int
+					count     int
+				}{index, 1}
+			}
+
+			if index+2 >= lenLine {
+				continue
+			}
+
+			if line[index] == line[index+2] {
+				niceLetterBetween = true
+			}
+		}
+
+		for _, value := range twoLetters {
+			if value.count >= 2 {
+				niceTwoLetters = true
+			}
+		}
+
+		if niceLetterBetween && niceTwoLetters {
+			quantitesNiceStrings++
+		}
+	}
+
+	return quantitesNiceStrings
+}
+
 func main() {
-	fmt.Println(puzzle1())
+	fmt.Println(puzzle2())
 }
